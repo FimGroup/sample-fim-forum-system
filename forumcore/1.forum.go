eddcb333bb4082c393a1c6651a017e133c7d29a1
goodplayer <meidomx@gmail.com>
2023-06-01 21:56:33 +0800

    Add initial files

---
+++ forumcore/1.forum.go
@@ -0,0 +1,88 @@
+package forumcore
+
+import (
+	"embed"
+	"log"
+
+	"github.com/FimGroup/fim/components"
+	"github.com/FimGroup/fim/fimapi/basicapi"
+	"github.com/FimGroup/fim/fimcore"
+)
+
+//go:embed flowmodel.*.toml
+var flowModelFs embed.FS
+
+//go:embed scene.*.toml
+var sceneFs embed.FS
+
+func StartForum() error {
+	container := fimcore.NewUseContainer()
+	if err := components.InitComponent(container); err != nil {
+		return err
+	}
+	if err := loadCustomFn(container, map[string]basicapi.FnGen{
+		"#print_obj": FnPrintObject,
+		"#panic":     FnPanic,
+	}); err != nil {
+		return err
+	}
+
+	if err := loadFlowModel(container, []string{
+		"flowmodel.all.toml",
+	}); err != nil {
+		return err
+	}
+	if err := loadMerged(container, []string{
+		"scene.user.register.toml",
+		"scene.user.login.toml",
+		"scene.forums.new_forum.toml",
+		"scene.posts.new_post.toml",
+	}); err != nil {
+		return err
+	}
+
+	if err := container.StartContainer(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func loadCustomFn(container basicapi.BasicContainer, mapping map[string]basicapi.FnGen) error {
+	for name, fg := range mapping {
+		if err := container.RegisterCustomFn(name, fg); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func loadFlowModel(container basicapi.BasicContainer, files []string) error {
+	for _, file := range files {
+		data, err := flowModelFs.ReadFile(file)
+		if err != nil {
+			return err
+		}
+		log.Println("read FlowModel content:", string(data))
+
+		if err := container.LoadFlowModel(string(data)); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func loadMerged(container basicapi.BasicContainer, files []string) error {
+	for _, file := range files {
+		data, err := sceneFs.ReadFile(file)
+		if err != nil {
+			return err
+		}
+		log.Println("read scene content:", string(data))
+
+		if err := container.LoadMerged(string(data)); err != nil {
+			return err
+		}
+	}
+	return nil
+}
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/FimGroup/sample-fim-forum-system/forumcore"
+)
+
+//go:embed version
+var ver string
+
+func main() {
+	fmt.Println("version:", strings.TrimSpace(string(ver)))
+
+	if err := forumcore.StartForum(); err != nil {
+		panic(err)
+	}
+
+	c := make(chan os.Signal)
+	signal.Notify(c, syscall.SIGHUP, syscall.SIGINT)
+	_ = <-c
+	fmt.Println("service exit!")
+}
